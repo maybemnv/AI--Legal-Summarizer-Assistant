1dd6c90e7c8215db0091324eed1c2efe7115667d
maybemnv <manavkauahal99@gmail.com>
2025-09-21 22:10:18 +0530

    Fixed Vite and vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-    fs: {
-      allow: ["./client", "./shared"],
-      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
-    },
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true, 
-  },
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./client"),
-      "@shared": path.resolve(__dirname, "./shared"),
-    },
-  },
+  build: {
+    outDir: "dist"
+  }
 });
